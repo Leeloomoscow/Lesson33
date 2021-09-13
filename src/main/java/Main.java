4c6949baf42e8cd2820c13b28292f84de1b26075
Aleksandra Rudenko <rudenkoay8@gmail.com>
2021-09-13 15:25:32 +0300

    Сортировка пузырьком

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,25 @@
 public class Main {
     public static void main(String[] args) {
+        int[] nums = {2, 1, 3, 6, 9, 4, 153, 0};
+        int temp; //Временное хранилище
+        boolean sorted = false;
+
+        while (!sorted) {
+            sorted = true;
+
+            for (int i = 0; i < nums.length - 1; i++) { //-1 пишем чтобы отнять одно значение чтобы не выйти за границы массива
+                if (nums[i] > nums[i + 1]) {
+                    temp = nums[i]; // Сохранение значения из нулевого индекса, чтобы не потерять 0
+                    nums[i] = nums[i + 1];
+                    nums[i + 1] = temp;
+                    sorted = false;
+
+                }
+            }
+        }
+        for (int i : nums) {
+            System.out.println(i);
+        }
     }
 }
 
